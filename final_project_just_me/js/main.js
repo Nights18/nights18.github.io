aa82d70fa74f8d3405755085e9de8965c3f868de
Nights18 <35713087+Nights18@users.noreply.github.com>
2018-03-18 21:51:22 -0400

    Homework&_Final Project Draft 2

--- final_project_just_me/js/main.js
+++ final_project_just_me/js/main.js
@@ -1,1 +1,26 @@
 // Make it rain!
+var imageNumber = 0;
+
+$('#next').on('click', function(){
+  if (imageNumber < 4){
+    imageNumber +=1;
+  } else {
+    imageNumber = 0;
+  }
+  console.log(imageNumber);
+
+  $('article').hide();
+  $('article').eq(imageNumber).show();
+})
+
+$('#previous').on('click', function(){
+  if (imageNumber < 4){
+    imageNumber -=1;
+  } else {
+    imageNumber = 4;
+  }
+  console.log(imageNumber);
+
+  $('article').hide();
+  $('article').eq(imageNumber).show();
+})
